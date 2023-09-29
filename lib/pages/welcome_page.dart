b9cdbe1ee8a7e4030544f9ad57bb56400c1b4ed4
Arpita Gupta <arpitagupta6620@gmail.com>
2023-09-29 22:16:04 +0530

    First Commit

---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key});
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  List images = [
+    "waterfall.png",
+    "pilgrimage.png",
+    "metal-crafts.png",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: images.length,
+          itemBuilder: (_, index) {
+            return Container(
+              width: double.maxFinite,
+              height: double.maxFinite,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("img/" + images[index]),
+                    fit: BoxFit.cover),
+              ),
+              child: Container(
+                margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
+                child: Row(
+                  children: [
+                    Column(
+                      children: [],
+                    )
+                  ],
+                ),
+              ),
+            );
+          }),
+    );
+  }
+}
